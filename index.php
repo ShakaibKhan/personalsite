e4d64f89b863c3468e4d54d1d10f3f7689ff45f1
Shakaib Khan <shakaib97@gmail.com>
2016-12-28 16:44:35 -0500

    First commit for my personal website

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lobster">
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<title>Shakaib Khan's Website</title>
+<h1>Shakaib Khan</h1>
+<div class="info_module" id="projects"><img src="images/wrench.png" height="42" width="42"></div>
+</body>
+</html>
